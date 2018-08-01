1cc591b83cf9949597a3558dac606c03b2e16ded
MonarchOne <41770811+MonarchOne@users.noreply.github.com>
2018-08-01 12:28:23 +0300

    Update markups.py

--- markups.py
+++ markups.py
@@ -3,3 +3,4 @@ from telebot import types
 markup_cat = types.ReplyKeyboardMarkup()
 markup_cat.row("Категория №1", 'Категория №2', 'Категория №3')
 markup_cat.row('Категория №4', 'Категория №5', 'Категория №6')
+markup_cat.row('Назад')
